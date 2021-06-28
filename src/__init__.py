1ba85c1b63141d52817117adc423e49e5a80be48
Zsolt Toth <tothzs87@gmail.com>
2021-06-28 13:55:34 +0200

    refactor: fix polynomial.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 Example module for template project.
 Pylint will check code in the src directory only!
 '''
-from src.Polynomial import Polynomial
+from src.polynomial import Polynomial
 
 __all__ = [
     'Polynomial'
